134ee87bbb9eb750e598ef6228589f0ead7f27af
DawnEggleton <dawn.eggleton@carleton.ca>
2022-02-13 10:23:17 -0500

    finish menus

--- TOTL22/js/functions.js
+++ TOTL22/js/functions.js
@@ -1,3 +1,14 @@
 function toggleMenu() {
     document.querySelector('body').classList.toggle('nav-open');
 }
+
+
+function highlightCode() {
+    console.log('clicked');
+    let clipcodeQuick = new Clipboard('.copyQuick', {
+        target: function(trigger) {
+            console.log(trigger.nextElementSibling);
+            return trigger.nextElementSibling.querySelector('textarea');
+        }
+    });
+}
--- TOTL22/js/scripts.js
+++ TOTL22/js/scripts.js
@@ -7,3 +7,45 @@ window.onscroll = function() {
         document.querySelector('.breadcrumb-nav').classList.remove('scrolled');
     }
 };
+
+
+//replace code arrows with html entities
+// Code Area
+$('.main-nav--code pre textarea').each(function() {
+    if($(this).text().trim().length > 0) {
+        $(this).text(
+            $(this).text().trim().replace(/<br>/ig, '').replace(/>/ig, '>').replace(/</ig, '<')
+        );
+    } else {
+        $(this).parent().parent().remove();
+    }
+});
+
+
+//calculate link scroll height
+let codeHeight = document.querySelector('.main-nav--content > .main-nav--code').clientHeight + 45;
+document.querySelector('.main-nav--links').style.height = `calc(100vh - ${codeHeight}px)`;
+
+
+//copy code to clipboard
+let clipboard = new Clipboard('.clipboard');
+clipboard.on('success', function(e) {
+    console.log(e);
+});
+clipboard.on('error', function(e) {
+    console.log(e);
+});
+let clipcode = new Clipboard('.codeclick', {
+    target: function(trigger) {
+    return trigger.nextElementSibling;
+    }
+});
+$("table[id='CODE-WRAP']").each(function() {
+    var cc = $(this).find("td[id='CODE']").html();
+
+    $(this).html(
+        "<div class='codeblock'><div class='c-title codeclick'><b>Copy to clipboard</b></div><div class='codecon'><pre><code>"
+        + cc +
+        "</code></pre></div></div>"
+    );
+});
