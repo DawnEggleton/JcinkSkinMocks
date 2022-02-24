7d7c00a3de29675b6756576e4e6f2b1e9ac89267
DawnEggleton <dawn.eggleton@carleton.ca>
2022-02-23 19:39:39 -0500

    fix errors, expand form functionality

--- TOTL22/js/functions.js
+++ TOTL22/js/functions.js
@@ -40,3 +40,91 @@ function capitalize(str, separators) {
     var regex = new RegExp('(^|[' + separators.join('') + '])(\\w)', 'g');
     return str.replace(regex, function(x) { return x.toUpperCase(); });
 }
+
+
+
+
+/* SORTING FORM FUNCTIONS */
+function addRequired(field) {
+    document.querySelector(field).setAttribute('required', true);
+}
+function removeRequired(field) {
+    document.querySelector(field).removeAttribute('required');   
+}
+function showFields(fields) {
+    document.querySelectorAll(fields).forEach(field => {
+        field.classList.remove('hide');
+    });
+}
+function hideFields(fields) {
+    document.querySelectorAll(fields).forEach(field => {
+        field.classList.add('hide');
+    });
+}
+function checkEmployed() {
+    let selected = document.querySelector('#sort-employed').options[document.querySelector('#sort-employed').selectedIndex].value;
+    if(selected == 'y') {
+        showFields('.ifJob');
+        addRequired('#sort-jobtitle');
+    } else {
+        hideFields('.ifJob');
+        removeRequired('#sort-jobtitle');
+    }
+}
+function checkUniversity() {
+    let selected = document.querySelector('#sort-universitystudent').options[document.querySelector('#sort-universitystudent').selectedIndex].value;
+    if(selected == 'y') {
+        showFields('.ifUni');
+        addRequired('#sort-universityname');
+        addRequired('#sort-universityprogram');
+        addRequired('#sort-universityyear');
+    } else {
+        hideFields('.ifUni');
+        removeRequired('#sort-universityname');
+        removeRequired('#sort-universityprogram');
+        removeRequired('#sort-universityyear');
+    }
+}
+function checkYear() {
+    let selected = document.querySelector('#sort-hogwartsyear').options[document.querySelector('#sort-hogwartsyear').selectedIndex].value;
+    switch (selected) {
+        case '1':
+        case '2':
+            showFields('.ifCore, .ifStart');
+            hideFields('.ifLower, .ifLeadershipershipPossible, .ifUpper, .ifElec');
+            break;
+        case '3':
+        case '4':
+            showFields('.ifLower, .ifElec, .ifCore');
+            hideFields('.ifLeadershipershipPossible, .ifUpper, .ifStart');
+            break;
+        case '5':
+            showFields('.ifLeadershipershipPossible, .ifLower, .ifElec, .ifCore');
+            hideFields('.ifUpper, .ifStart');
+            break;
+        case '6':
+        case '7':
+            showFields('.ifLeadershipershipPossible, .ifUpper, .ifElec');
+            hideFields('.ifLower, .ifStart, .ifCore');
+            break;
+        default:
+            hideFields('.ifLower, .ifUpper, .ifLeadership, .ifLeadershipershipPossible, .ifQuidditch, .ifElec, .ifStart, .ifCore');
+            break;
+    } 
+}
+function checkLeadPos() {
+    let selected = document.querySelector('#sort-leadership').options[document.querySelector('#sort-leadership').selectedIndex].value;
+    if(selected == 'y') {
+        showFields('.ifLeadership');
+    } else {
+        hideFields('.ifLeadership');
+    }  
+}
+function checkQuid() {
+    let selected = document.querySelector('#sort-quidditch').options[document.querySelector('#sort-quidditch').selectedIndex].value;
+    if(selected == 'y') {
+        showFields('.ifQuidditch');
+    } else {
+        hideFields('.ifQuidditch');
+    }  
+}
