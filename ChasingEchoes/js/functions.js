27a7c93bae71b0b56927cf284f6bf4a7b0a613e5
Dawn Eggleton <dawn.eggleton@carleton.ca>
2022-07-22 08:00:32 -0300

    minor updates and site title added

--- ChasingEchoes/js/functions.js
+++ ChasingEchoes/js/functions.js
@@ -6,20 +6,17 @@ function setBrightness() {
                 document.querySelector('body').classList.remove('light');
                 document.querySelector('body').classList.remove('medium');
                 document.querySelector('body').classList.add('dark');
-                document.querySelectorAll('input[name="setting-brightness]').forEach(slider => slider.value = 1);
                 break;
             case '2':
                 document.querySelector('body').classList.remove('light');
                 document.querySelector('body').classList.add('medium');
                 document.querySelector('body').classList.remove('dark');
-                document.querySelectorAll('input[name="setting-brightness]').forEach(slider => slider.value = 2);
                 break;
             case '3':
             default:
                 document.querySelector('body').classList.add('light');
                 document.querySelector('body').classList.remove('medium');
                 document.querySelector('body').classList.remove('dark');
-                document.querySelectorAll('input[name="setting-brightness]').forEach(slider => slider.value = 1);
                 break;
         }
     } else {
@@ -27,16 +24,13 @@ function setBrightness() {
             document.querySelector('body').classList.remove('light');
             document.querySelector('body').classList.remove('medium');
             document.querySelector('body').classList.add('dark');
-            document.querySelectorAll('input[name="setting-brightness]').forEach(slider => slider.value = 1);
-            localStorage.setItem('brightness', '1');
         } else {
             document.querySelector('body').classList.add('light');
             document.querySelector('body').classList.remove('medium');
             document.querySelector('body').classList.remove('dark');
-            document.querySelectorAll('input[name="setting-brightness]').forEach(slider => slider.value = 3);
-            localStorage.setItem('brightness', '3');
         }
     }
+    document.querySelectorAll('input[name="setting-brightness"]').forEach(slider => slider.value = localStorage.getItem('brightness'));
 }
 function setSize() {
     if(localStorage.getItem('size') !== null) {
@@ -45,30 +39,25 @@ function setSize() {
                 document.querySelector('body').classList.add('smFont');
                 document.querySelector('body').classList.remove('mdFont');
                 document.querySelector('body').classList.remove('lgFont');
-                document.querySelectorAll('input[name="setting-size]').forEach(slider => slider.value = 1);
-                document.querySelectorAll('input[name="setting-size]').forEach(slider => slider.value = 1);
                 break;
             case '2':
                 document.querySelector('body').classList.remove('smFont');
                 document.querySelector('body').classList.add('mdFont');
                 document.querySelector('body').classList.remove('lgFont');
-                document.querySelectorAll('input[name="setting-size]').forEach(slider => slider.value = 2);
                 break;
             case '3':
             default:
                 document.querySelector('body').classList.remove('smFont');
                 document.querySelector('body').classList.remove('mdFont');
                 document.querySelector('body').classList.add('lgFont');
-                document.querySelectorAll('input[name="setting-size]').forEach(slider => slider.value = 3);
                 break;
         }
     } else {
         document.querySelector('body').classList.add('smFont');
         document.querySelector('body').classList.remove('mdFont');
         document.querySelector('body').classList.remove('lgFont');
-        document.querySelectorAll('input[name="setting-size]').forEach(slider => slider.value = 1);
-        localStorage.setItem('size', '1');
     }
+    document.querySelectorAll('input[name="setting-size"]').forEach(slider => slider.value = localStorage.getItem('size'));
 }
 
 function toggleMenu(e) {
